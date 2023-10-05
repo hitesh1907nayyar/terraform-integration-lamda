67565625db17cb68b7f27369b6192a5ac4e79ce9
neetesh.yadav <neetesh.yadav@bigohtech.com>
2023-10-06 00:12:59 +0530

    initial commit

---
+++ module/lamda/variable.tf
@@ -0,0 +1,19 @@
+variable "function_name" {
+  description = "Name of the Lambda function"
+}
+
+variable "handler" {
+  description = "Lambda function handler"
+}
+
+variable "runtime" {
+  description = "Lambda function runtime"
+}
+
+variable "filename" {
+  description = "Path to the Lambda function code package (ZIP or folder)"
+}
+
+variable "iam_role_name" {
+  description = "Name of the IAM role for Lambda execution"
+}
