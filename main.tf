f679d50af3411a8335a4b53fc7c73f40eece8036
devopsofworld <145892752+devopsofworld@users.noreply.github.com>
2023-10-06 11:04:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "neeteshterraformtfstatemanagebucket"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+  }
+}
 provider "aws" {
   region = var.aws_region
 }
