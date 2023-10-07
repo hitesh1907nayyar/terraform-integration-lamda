685f242f96a1d376df34573adc560bff86f1865a
neetesh.yadav <neetesh.yadav@bigohtech.com>
2023-10-07 23:42:20 +0530

    update private resource

--- terraform/module/s3/main.tf
+++ terraform/module/s3/main.tf
@@ -6,10 +6,10 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket = aws_s3_bucket.s3_bucket.id
+# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+#   bucket = aws_s3_bucket.s3_bucket.id
 
-  # Set the ACL rules here, for example:
-  acl = "private"
+#   # Set the ACL rules here, for example:
+#   acl = "private"
 
-}
+# }
