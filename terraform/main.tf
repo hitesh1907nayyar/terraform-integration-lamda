5db5b0c1837c27cf51700510b229badcae1edaef
hitesh1907nayyar <42111008+hitesh1907nayyar@users.noreply.github.com>
2023-10-15 18:01:41 +0100

    UpdateFile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "neeteshterraformtfstatemanagebucket"
+    bucket         = "devopsofworldbucketterraformstatedev"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
   }
